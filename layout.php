3b2aad0f6f8dcf38e5aea60f074004ee55d5cffe
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 01:18:31 -0500

    added new dark theme

--- layout.php
+++ layout.php
@@ -10,12 +10,23 @@ include($_SERVER['DOCUMENT_ROOT'] . '/config/variables.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title . ' | ' . $APP_NAME ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://bootswatch.com/5/cyborg/bootstrap.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <style>
+        body {
+            font-family: 'Montserrat', Roboto, -apple-system, BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif;
+            font-weight: 500;
+        }
+
+        button {
+            font-weight: 500 !important;
+        }
+    </style>
     <?php
-    if (IsVariableSet($css)) {
+    if (IsVariableSet(@$css)) {
         echo $css;
     } else {
         // css is not present and nothing will happen
@@ -25,24 +36,24 @@ include($_SERVER['DOCUMENT_ROOT'] . '/config/variables.php');
 </head>
 
 <body>
-    <header class="p-3 bg-dark text-white">
+    <header class="p-3 bg-info">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a class="nav-link px-2 text-white"><?php echo $APP_NAME ?></a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Home</a></li>
+                    <li><a href="/" class="nav-link px-2 text-white">Home</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Forum</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Users</a></li>
-                    <li><a href="#" class="nav-link px-2 text-white">Updates</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Updates <span class="badge bg-warning">New</span></a></li>
                 </ul>
 
                 <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-                    <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
+                    <input type="search" class="form-control form-control-dark" placeholder="Search <?php echo $APP_NAME ?>..." aria-label="Search">
                 </form>
 
                 <div class="text-end">
-                    <button type="button" class="btn btn-outline-light me-2">Login</button>
-                    <button type="button" class="btn btn-warning">Sign-up</button>
+                    <button type="button" class="btn btn-warning me-2 border-3" onclick="document.location.href='/login';">Login</button>
+                    <button type="button" class="btn btn-warning border-3">Sign-up</button>
                 </div>
             </div>
         </div>
--- views/_index.php
+++ views/_index.php
@@ -1,2 +1,5 @@
 <h1 class="text-center">Welcome to <?php echo $APP_NAME ?>!</h1>
 <h4 class="text-center fw-normal">Website Build #<?php echo $APP_BUILD ?> &bullet; <?php echo $APP_BUILD_DATE ?></h4>
+<h6 class="text-center fw-normal"><a href="https://github.com/iwannaplayminicraft/php-roblox-clone">Github Repository</a>
+    &bullet; <a href="https://elfo.neocities.org/">Neocities</a>
+    &bullet; <a href="https://twitter.com/elfofthestorm">Twitter</a></h6>
