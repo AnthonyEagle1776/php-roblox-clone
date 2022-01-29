91b0833e769461b2c3f7e073744919c0dc14ba96
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 01:40:17 -0500

    login form to dark mode

--- views/guest/_login.php
+++ views/guest/_login.php
@@ -1,24 +1,24 @@
 <div class="row">
     <div class="col-5" style="margin: auto;">
-        <div class="card">
-            <div class="card-header">
+        <div class="card border-0">
+            <div class="card-header bg-warning text-white">
                 Login
             </div>
             <div class="card-body">
-                <form>
+                <form autocomplete="false">
                     <div class="mb-3">
                         <label for="Email" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="Email">
+                        <input type="email" class="form-control bg-secondary text-white" id="Email">
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1">
+                        <input type="password" class="form-control bg-secondary text-white" id="exampleInputPassword1">
                     </div>
                     <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <input type="checkbox" class="form-check-input bg-secondary text-white" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Remember Me</label>
                     </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-warning">Submit</button>
                 </form>
             </div>
         </div>
