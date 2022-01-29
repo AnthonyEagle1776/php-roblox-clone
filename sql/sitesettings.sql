3b6d363a808c68475e50d05d54e5d9cf3e757379
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 00:56:28 -0500

    updated for removal of ID param

--- sql/sitesettings.sql
+++ sql/sitesettings.sql
@@ -10,3 +10,7 @@ CREATE TABLE sitesettings (
 /* The defualt settings template with a stock alert and site maintenance disabled. */
 INSERT INTO `sitesettings` (`alert_enabled`, `site_maintenance`, `alert_message`, `alert_color`) VALUES
 (1, 0, 'RobloxClone is in development!', 'primary');
+
+/* For older users of this repository, if your sitesettings shipped with an ID run this SQL command */
+
+ALTER TABLE `sitesettings` DROP `id`;
