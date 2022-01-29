d4db5cb2055f224666aa784f9228532c44eac8a5
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 15:36:19 -0500

    updated alert boolean variable name

--- config/checks.php
+++ config/checks.php
@@ -3,7 +3,7 @@
 $getSiteSettings = "SELECT * FROM sitesettings";
 $result = mysqli_query($conn, $getSiteSettings);
 // alert variables
-$AlertEnabled;
+$Alert;
 $AlertMessage;
 $AlertColor;
 $MaintenanceMode;
@@ -11,7 +11,7 @@ $MaintenanceMode;
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while ($row = mysqli_fetch_assoc($result)) {
-        $AlertEnabled = $row['alert_enabled'];
+        $Alert = $row['alert_enabled'];
         $AlertMessage = $row['alert_message'];
         $AlertColor = $row['alert_color'];
         $MaintenanceMode = $row['site_maintenance'];
