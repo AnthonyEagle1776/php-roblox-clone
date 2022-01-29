3ac6c64f9901ba307d158c3964a9c94d5847231a
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 01:17:36 -0500

    database name no longer locked to app name

--- config/env.php
+++ config/env.php
@@ -7,7 +7,7 @@ $APP_URL = 'localhost';
 $APP_BUILD = 1;
 $APP_BUILD_DATE = 'January 28th, 2022';
 // database variables
-$DATABASE = $APP_NAME;
+$DATABASE = 'RobloxClone';
 $DATABASE_HOST = $APP_URL;
 $DATABASE_USERNAME = 'root';
 $DATABASE_PASSWORD = '';
