00ad7d9600e63281537afb56c179fcb95156cf51
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-29 01:18:13 -0500

    added login

---
+++ login/index.php
@@ -0,0 +1,4 @@
+<?php
+$title = 'Login';
+$childView = $_SERVER['DOCUMENT_ROOT'] . "/views/guest/_login.php";
+include($_SERVER['DOCUMENT_ROOT'] . '/layout.php');
---
+++ views/guest/_login.php
@@ -0,0 +1,26 @@
+<div class="row">
+    <div class="col-5" style="margin: auto;">
+        <div class="card">
+            <div class="card-header">
+                Login
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="mb-3">
+                        <label for="Email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="Email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                        <label class="form-check-label" for="exampleCheck1">Remember Me</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
