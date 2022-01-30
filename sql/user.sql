a6a82a6e8ad9714f9d2b7ab3c8a13b399053ec9f
fanch__ <averyalexandercarroll08@gmail.com>
2022-01-30 01:02:51 -0500

    updated sql file

--- sql/user.sql
+++ sql/user.sql
@@ -7,15 +7,6 @@
     updated_at: Used to display if the user is online or offline. Everytime the user clicks on a page a function runs that updates this variable.
 
     */
-<<<<<<< HEAD
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(254) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME
-);
-=======
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -24,4 +15,3 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT current_timestamp(),
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
->>>>>>> 36744933d07f34c104226be579c3b01d964d0c32
