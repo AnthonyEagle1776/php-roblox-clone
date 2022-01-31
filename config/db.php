e0fd66389e25a276ac390088baa4efad1a69f913
Fanch__ <averyalexandercarroll08@gmail.com>
2022-01-31 05:33:50 -0500

    Changed error 500 to HTML file instead of PHP.

--- config/db.php
+++ config/db.php
@@ -5,6 +5,6 @@ date_default_timezone_set('America/New_York');
 try {
     $conn = OpenConnection($DATABASE_HOST, $DATABASE_USERNAME, $DATABASE_PASSWORD, $DATABASE);
 } catch (mysqli_sql_exception $e) {
-    include($_SERVER['DOCUMENT_ROOT'] . '/views/error/500.php');
+    include($_SERVER['DOCUMENT_ROOT'] . '/views/error/500.html');
     exit();
 }
